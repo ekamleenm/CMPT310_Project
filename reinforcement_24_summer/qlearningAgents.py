10ecabcc0e1cd4eda045438c2cfce84e3597eae8
ekamleen <ekamleenm@gmail.com>
2024-07-15 14:10:14 -0700

    q5 tested

--- reinforcement_24_summer/qlearningAgents.py
+++ reinforcement_24_summer/qlearningAgents.py
@@ -15,8 +15,8 @@
 from game import *
 from learningAgents import ReinforcementAgent
 
+import random, util, math
 
-import random,util,math
 
 class QLearningAgent(ReinforcementAgent):
     """
@@ -38,13 +38,11 @@ class QLearningAgent(ReinforcementAgent):
         - self.getLegalActions(state)
           which returns legal actions for a state
     """
+
     def __init__(self, **args):
         "You can initialize Q-values here..."
         ReinforcementAgent.__init__(self, **args)
-
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
+        self.qValues = util.Counter()  # Initialize Q-values
 
     def getQValue(self, state, action):
         """
@@ -53,9 +51,7 @@ class QLearningAgent(ReinforcementAgent):
           or the Q node value otherwise
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
-
+        return self.qValues[(state, action)]
 
     def computeValueFromQValues(self, state):
         """
@@ -65,18 +61,34 @@ class QLearningAgent(ReinforcementAgent):
           terminal state, you should return a value of 0.0.
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
-
+        legalActions = self.getLegalActions(state)
+        if not legalActions:
+            return 0.0
+        return max([self.getQValue(state, action) for action in legalActions])
 
     def computeActionFromQValues(self, state):
         """
-          Compute the best action to take in a state.  Note that if there
-          are no legal actions, which is the case at the terminal state,
-          you should return None.
-        """
+              Compute the best action to take in a state.  Note that if there
+              are no legal actions, which is the case at the terminal state,
+              you should return None.
+            """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        legalActions = self.getLegalActions(state)
+        if not legalActions:
+            return None
 
+        best_action = None
+        max_value = float('-inf')
+
+        for action in legalActions:
+            q_value = self.getQValue(state, action)
+            if q_value > max_value:
+                max_value = q_value
+                best_action = action
+            elif q_value == max_value:
+                best_action = random.choice([best_action, action])
+
+        return best_action
 
     def getAction(self, state):
         """
@@ -92,10 +104,17 @@ class QLearningAgent(ReinforcementAgent):
         # Pick Action
         legalActions = self.getLegalActions(state)
         action = None
-        "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        if not legalActions:
+            return None
 
+        if util.flipCoin(self.epsilon):
+            # Choose a random action with probability epsilon
+            action = random.choice(legalActions)
+        else:
+            # Choose the best action based on current Q-values
+            action = self.computeActionFromQValues(state)
 
+        return action
 
     def update(self, state, action, nextState, reward):
         """
@@ -107,7 +126,10 @@ class QLearningAgent(ReinforcementAgent):
           it will be called on your behalf
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        q_value = self.getQValue(state, action)
+        next_value = self.computeValueFromQValues(nextState)
+        sample = reward + self.discount * next_value
+        self.qValues[(state, action)] = (1 - self.alpha) * q_value + self.alpha * sample
         """
               QLearning update algorithm:
 
@@ -117,11 +139,8 @@ class QLearningAgent(ReinforcementAgent):
 
         """
 
-
-
     def getPolicy(self, state):
         return self.computeActionFromQValues(state)
 
     def getValue(self, state):
         return self.computeValueFromQValues(state)
-
